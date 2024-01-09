7f0026d3d0ae0418384d150f62a301ea8c6891bd
vyPal <66716025+vyPal@users.noreply.github.com>
2024-01-09 15:49:03 +0100

    Add dashboard

---
+++ pages/dashboard/vypaljeborec.tsx
@@ -0,0 +1,25 @@
+import { QueryResult, QueryResultRow, sql } from "@vercel/postgres";
+import "../../app/globals.css"
+import { GrTest, GrMonitor } from "react-icons/gr"
+
+export default function Page({ data, error } : { data: QueryResult<QueryResultRow>, error: Error }) {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-zinc-950 dark:bg-white text-white">
+      {data.rows.map(row => 
+        <div key={row.username}>
+          {row.type == "test" ? <GrTest style={{"display": "inline"}}/> : <GrMonitor style={{"display": "inline"}}/>}
+          <p style={{"display": "inline"}}>&nbsp;{row.username} is at PC {row.pc} on checkpoint {row.checkpoint} with {row.history} history</p>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export async function getServerSideProps() {
+  try {
+    let res = await sql`SELECT username, checkpoint, history, pc, type FROM users ORDER BY type DESC`
+    return { props: { data: res } }
+  } catch (err) {
+    return { props: { data: null, error: err } }
+  }
+}
