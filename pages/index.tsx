552d4d3fd8a33e02b2474321a23961f3434de1a2
vyPal <kubik.palacky@gmail.com>
2025-02-03 19:05:16 +0100

    Some updates...

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "../app/globals.css";
-import { motion, useMotionValue, useAnimation } from 'framer-motion';
+import { motion, useMotionValue, useAnimation } from "framer-motion";
 import { LoginForm } from "@/components/custom/login";
 
 const HomePage = () => {
@@ -11,7 +11,7 @@ const HomePage = () => {
 
   useEffect(() => {
     controls.start({ opacity: 1 });
-    const targetDate = new Date("2024-02-15T08:55:00+01:00"); // TODO nastavit správný čas
+    const targetDate = new Date("2025-02-05T09:00:00+01:00"); // TODO nastavit správný čas
     const currentDate = new Date();
 
     if (currentDate >= targetDate) {
@@ -20,7 +20,19 @@ const HomePage = () => {
       sessionStorage.removeItem("imanok");
     }
 
-    let konamiCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a', 'Enter'];
+    let konamiCode = [
+      "ArrowUp",
+      "ArrowUp",
+      "ArrowDown",
+      "ArrowDown",
+      "ArrowLeft",
+      "ArrowRight",
+      "ArrowLeft",
+      "ArrowRight",
+      "b",
+      "a",
+      "Enter",
+    ];
     let konamiCodePosition = 0;
 
     const keydownHandler = (e: any) => {
@@ -30,29 +42,34 @@ const HomePage = () => {
         konamiCodePosition++;
 
         switch (key) {
-          case 'ArrowUp':
+          case "ArrowUp":
             controls.start({ translateY: -50 });
             break;
-          case 'ArrowDown':
+          case "ArrowDown":
             controls.start({ translateY: 50 });
             break;
-          case 'ArrowLeft':
+          case "ArrowLeft":
             controls.start({ translateX: -50 });
             break;
-          case 'ArrowRight':
+          case "ArrowRight":
             controls.start({ translateX: 50 });
             break;
-          case 'b':
-          case 'a':
+          case "b":
+          case "a":
             controls.start({ scale: 1.15 });
             break;
-          case 'Enter':
+          case "Enter":
             controls.start({ scale: 0.85, opacity: 0 });
             break;
         }
 
         setTimeout(() => {
-          controls.start({ translateY: 0, translateX: 0, scale: 1, opacity: 1 });
+          controls.start({
+            translateY: 0,
+            translateX: 0,
+            scale: 1,
+            opacity: 1,
+          });
         }, 50);
 
         if (konamiCodePosition === konamiCode.length) {
@@ -66,10 +83,10 @@ const HomePage = () => {
       }
     };
 
-    document.addEventListener('keydown', keydownHandler);
+    document.addEventListener("keydown", keydownHandler);
 
     return () => {
-      document.removeEventListener('keydown', keydownHandler);
+      document.removeEventListener("keydown", keydownHandler);
     };
   }, [controls]);
 
@@ -79,17 +96,22 @@ const HomePage = () => {
         <motion.div
           initial={{ opacity: 0 }}
           animate={controls}
-          transition={{ type: 'spring', stiffness: 300, damping: 20 }}
+          transition={{ type: "spring", stiffness: 300, damping: 20 }}
         >
           <div className="p-6 max-w-sm mx-auto bg-gray-800 rounded-xl shadow-md flex items-center space-x-4">
             <div>
-              <div className="text-xl font-medium text-white">Soutěž ještě nebyla zahájena</div>
-              <p className="text-gray-400">Prosíme počkejte na zahájení. Pokud se snažíte aplikaci testovat, tak víte co dělat ; )</p>
+              <div className="text-xl font-medium text-white">
+                Soutěž ještě nebyla zahájena
+              </div>
+              <p className="text-gray-400">
+                Prosíme počkejte na zahájení. Pokud se snažíte aplikaci
+                testovat, tak víte co dělat ; )
+              </p>
             </div>
           </div>
         </motion.div>
       </div>
-    )
+    );
   }
 
   return (
